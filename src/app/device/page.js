eec0d37f0cb89b87586af8af5f45b66daf7bf13b
w3App <kshatriyakabawat@gmail.com>
2023-09-11 14:59:12 +0530

    udpate

--- src/app/device/page.js
+++ src/app/device/page.js
@@ -1,32 +1,10 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import UAParser from 'ua-parser-js';
-
-const DeviceInfo = () => {
-    const [deviceInfo, setDeviceInfo] = useState(null);
-    const [isLoad, setIsLoad] = useState(false)
-    useEffect(() => {
-        const parser = new UAParser();
-        const result = parser.getResult();
-        setDeviceInfo(result);
-    }, []);
-
-    useEffect(() => {
-        if (deviceInfo?.os?.name === 'Windows') {
-            setIsLoad(true)
-        } else {
-            setIsLoad(false)
-        }
-    }, [deviceInfo]);
-    return (
-
-        isLoad ? <>
-            welcome to event {deviceInfo?.os?.name}
-        </> : <>
-            {deviceInfo?.os?.name}
-        </>
-
-    );
-};
-
-export default DeviceInfo;
+// "use client"
+import { isMobile } from 'react-device-detect';
+const Device = () => {
+    if (isMobile) {
+        return <div> This content is available only on mobile</div>
+    } else {
+        return <div> content </div>
+    }
+}
+export default Device
